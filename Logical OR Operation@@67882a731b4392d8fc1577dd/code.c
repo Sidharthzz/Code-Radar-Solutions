cb8823478c8eae6892a648250bd3d63daa0226b3
Sidharthzz <sidharth1728.becse24@chitkara.edu.in>
2025-01-31 12:08:22 +0530

    Solution to Logical OR Operation

--- Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
+++ Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
@@ -2,10 +2,10 @@
 int main() {
    int num1 , num2;
    if(scanf("%d %d", &num1 , &num2) == 2);
-     if(num1 > 0){
+     if(num1 > 0 || num2>0){
          printf("True\n");
 
-     }  else if(num1==0){
+     }  else if(num1==0 || num2==0){
          printf("zero\n");
      }
      else{
