919ba969102799a74e3c8eee7e4982035d8594d9
Sidharthzz <sidharth1728.becse24@chitkara.edu.in>
2025-01-31 15:25:18 +0530

    Solution to Check MSB of a Number

---
+++ Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+   int num;
+   printf("Enter a number;");
+   scanf("%d", &num);
+
+   int p = 0;
+   while((num & 1)==0){
+     num>>=1;
+     p++;
+
+   }
+   printf("%d\n",p)
+    return 0;
+}
