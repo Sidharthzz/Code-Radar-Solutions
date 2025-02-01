fa5e6dcf5a160bba60aacb0fb9cd7ff36dcacad1
Sidharthzz <sidharth1728.becse24@chitkara.edu.in>
2025-02-01 19:20:33 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() { 
+    char a;
+    scanf ("%c",&c);
+    if(c == 'A'){
+        printf("Excellent\n");
+    } else if ( c=='B'){
+      printf("Good");
+    }else if (c == 'C'){
+        printf("Average")
+    }
+
+    return 0;
+}
